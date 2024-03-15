f1fc20a6e9bc7f98893be7d414e6624cf17b5bc9
error4071 <sergey_tsoy87@mail.ru>
2024-03-16 01:01:23 +0500

    Lombok edit

--- src/main/java/hexlet/code/dto/UserCreateDTO.java
+++ src/main/java/hexlet/code/dto/UserCreateDTO.java
@@ -3,12 +3,13 @@ package hexlet.code.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserCreateDTO {
 
     private String firstName;
--- src/main/java/hexlet/code/dto/UserDTO.java
+++ src/main/java/hexlet/code/dto/UserDTO.java
@@ -1,13 +1,14 @@
 package hexlet.code.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserDTO {
 
     private long id;
