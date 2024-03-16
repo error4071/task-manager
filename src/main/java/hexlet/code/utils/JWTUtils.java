a51da2192da57dee48b018c95dd5e55d5afa002c
error4071 <sergey_tsoy87@mail.ru>
2024-03-16 23:19:52 +0500

    Method add

--- src/main/java/hexlet/code/utils/JWTUtils.java
+++ src/main/java/hexlet/code/utils/JWTUtils.java
@@ -1,13 +1,17 @@
 package hexlet.code.utils;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
+@Component
+@AllArgsConstructor
 public class JWTUtils {
 
     @Autowired
