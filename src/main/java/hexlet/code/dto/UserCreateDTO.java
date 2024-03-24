f048cc782d6c3286367ec9db8de4aaef6104aad1
error4071 <sergey_tsoy87@mail.ru>
2024-03-24 19:49:52 +0500

    Minor fix

--- src/main/java/hexlet/code/dto/UserCreateDTO.java
+++ src/main/java/hexlet/code/dto/UserCreateDTO.java
@@ -3,7 +3,6 @@ package hexlet.code.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
