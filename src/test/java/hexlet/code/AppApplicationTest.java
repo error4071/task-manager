06024448bfd9e87c3fe5fcb25e3037a9191770db
error4071 <sergey_tsoy87@mail.ru>
2024-04-03 21:47:59 +0500

    AppApplicationTest add

--- src/test/java/hexlet/code/AppApplicationTest.java
+++ src/test/java/hexlet/code/AppApplicationTest.java
@@ -1,1 +1,64 @@
+package hexlet.code;
 
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Map;
+
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import hexlet.code.util.ModelGenerator;
+import net.datafaker.Faker;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AppApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Faker faker;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelGenerator modelGenerator;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private JwtRequestPostProcessor token;
+
+    private User testUser;
+
+    @BeforeEach
+    public void setUp() {
+        token = jwt().jwt(builder -> builder.subject("hexlet@Example.com"));
+        testUser = Instancio.of(modelGenerator.getUserModel())
+                .create();
+        userRepository.save(testUser);
+    }
+
+    @Test
+    void contextLoads() {
+    }
+}
