90ba0757206026d3386c09e847de770e5efa399b
error4071 <sergey_tsoy87@mail.ru>
2024-04-03 22:14:22 +0500

    AppApplicationTest fix3

--- src/test/java/hexlet/code/AppApplicationTest.java
+++ src/test/java/hexlet/code/AppApplicationTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -50,6 +51,7 @@ public class AppApplicationTest {
         userRepository.save(testUser);
     }
 
+    @Test
     public void testIndex() throws Exception {
         var request = get("/api/users").with(token);
         var result = mockMvc.perform(request)
