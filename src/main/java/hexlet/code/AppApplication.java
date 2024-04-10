1fab64204dbcbe4f7f086a028450a2d081bcd982
error4071 <sergey_tsoy87@mail.ru>
2024-04-10 22:40:02 +0500

    Bean Faker add

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,8 +1,10 @@
 package hexlet.code;
 
+import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -13,5 +15,10 @@ public class AppApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @Bean
+    public Faker getFaker() {
+        return new Faker();
     }
 }
