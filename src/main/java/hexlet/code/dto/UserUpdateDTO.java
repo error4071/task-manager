0941a06c6a057138748a24d1adb92862fdead519
error4071 <sergey_tsoy87@mail.ru>
2024-04-10 23:12:30 +0500

    fix

--- src/main/java/hexlet/code/dto/UserUpdateDTO.java
+++ src/main/java/hexlet/code/dto/UserUpdateDTO.java
@@ -3,14 +3,12 @@ package hexlet.code.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.openapitools.jackson.nullable.JsonNullable;
 
 @Setter
 @Getter
-@AllArgsConstructor
 public class UserUpdateDTO {
 
     @Email
