96bf1c6088b2e26c0e7d6717ab3dc47bf5f19ead
error4071 <sergey_tsoy87@mail.ru>
2024-04-15 23:04:43 +0500

    test taskStatus fix

--- src/test/java/hexlet/code/UserTest.java
+++ src/test/java/hexlet/code/UserTest.java
@@ -133,6 +133,6 @@ public class UserTest {
         mockMvc.perform(request)
                 .andExpect(status().isNoContent());
 
-        assertThat(userRepository.existsById(testUser.getId())).isEqualTo(true);
+        assertThat(userRepository.existsById(testUser.getId())).isEqualTo(false);
     }
 }
