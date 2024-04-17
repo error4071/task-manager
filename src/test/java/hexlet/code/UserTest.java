d8658567dc08f20b0ab4a55440f0ad1bc460268e
error4071 <sergey_tsoy87@mail.ru>
2024-04-18 00:42:27 +0500

    taskstatus test

--- src/test/java/hexlet/code/UserTest.java
+++ src/test/java/hexlet/code/UserTest.java
@@ -20,6 +20,7 @@ import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
