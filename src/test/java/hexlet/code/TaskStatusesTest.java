95a5778b8384f754bb41e1e2ee7cdb7286cece4f
error4071 <sergey_tsoy87@mail.ru>
2024-04-17 23:58:45 +0500

    taskstatus test

--- src/test/java/hexlet/code/TaskStatusesTest.java
+++ src/test/java/hexlet/code/TaskStatusesTest.java
@@ -1,1 +1,72 @@
+package hexlet.code;
 
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import hexlet.code.mapper.TaskStatusMapper;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.repository.TaskStatusRepository;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hexlet.code.util.ModelGenerator;
+import net.datafaker.Faker;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TaskStatusesTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Faker faker;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private ModelGenerator modelGenerator;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private JwtRequestPostProcessor token;
+
+    private TaskStatus testTaskStatus;
+
+    private TaskStatusMapper taskStatusMapper;
+
+    @BeforeEach
+    public void setUp() {
+        token = jwt().jwt(builder -> builder.subject("hexlet@Example.com"));
+        testTaskStatus = Instancio.of(modelGenerator.getTaskStatusModel())
+                .create();
+        taskStatusRepository.save(testTaskStatus);
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        var request = get("/api/task_statuses").with(token);
+        var result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).isArray();
+        assertThat(body).contains(String.valueOf(testTaskStatus.getId()));
+        assertThat(body).contains(testTaskStatus.getName());
+        assertThat(body).contains(testTaskStatus.getSlug());
+    }
+}
