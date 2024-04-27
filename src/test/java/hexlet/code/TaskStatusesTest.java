0521dee7d358314e5d6525659f96975f6b563121
error4071 <sergey_tsoy87@mail.ru>
2024-04-28 01:17:18 +0500

    Task status test add

--- src/test/java/hexlet/code/TaskStatusesTest.java
+++ src/test/java/hexlet/code/TaskStatusesTest.java
@@ -3,11 +3,14 @@ package hexlet.code;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import hexlet.code.dto.TaskStatus.TaskStatusCreateDTO;
+import hexlet.code.dto.User.UserCreateDTO;
 import hexlet.code.mapper.TaskStatusMapper;
 import hexlet.code.model.TaskStatus;
+import hexlet.code.model.User;
 import hexlet.code.repository.TaskStatusRepository;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,10 +26,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import hexlet.code.util.ModelGenerator;
 import net.datafaker.Faker;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.Optional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TaskStatusesTest {
+
+    private static final Faker FAKER = new Faker();
 
     @Autowired
     private MockMvc mockMvc;
@@ -72,5 +81,37 @@ public class TaskStatusesTest {
         assertThat(body).contains(String.valueOf(testTaskStatus.getId()));
         assertThat(body).contains(testTaskStatus.getName());
         assertThat(body).contains(testTaskStatus.getSlug());
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        var dto = taskStatusMapper.map(testTaskStatus);
+
+        var request = post("/api/task_statuses")
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto));
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated());
+
+        var taskStatusTest = taskStatusRepository.findBySlug(dto.getSlug()).get();
+
+        assertThat(taskStatusTest).isNotNull();
+        assertThat(taskStatusTest.getName()).isEqualTo(dto.getName());
+    }
+
+    @Test
+    public void testDestroy() throws Exception {
+
+        taskStatusRepository.save(testTaskStatus);
+
+        var request = delete("/api/task_statuses/{id}", testTaskStatus.getId()).with(token);
+
+        mockMvc.perform(request)
+                .andExpect(status().isNoContent());
+
+        Optional<TaskStatus> userOptional = taskStatusRepository.findBySlug(testTaskStatus.getSlug());
+        assertThat(userOptional.isEmpty()).isEqualTo(true);
     }
 }
