82e27425ed52828b90c7b509bd579fabed9e67fb
error4071 <sergey_tsoy87@mail.ru>
2024-04-28 23:31:23 +0500

    TaskTest update

--- src/test/java/hexlet/code/LabelTest.java
+++ src/test/java/hexlet/code/LabelTest.java
@@ -1,4 +1,10 @@
 package hexlet.code;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class LabelTest {
+
 }
---
+++ src/test/java/hexlet/code/TaskTest.java
@@ -0,0 +1,74 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.mapper.TaskMapper;
+import hexlet.code.mapper.TaskStatusMapper;
+import hexlet.code.model.Task;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.repository.TaskRepository;
+import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.util.ModelGenerator;
+import net.datafaker.Faker;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TaskTest {
+    private static final Faker FAKER = new Faker();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Faker faker;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private ModelGenerator modelGenerator;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor token;
+
+    private Task testTask;
+
+    private TaskMapper taskMapper;
+
+    @BeforeEach
+    public void setUp() {
+        token = jwt().jwt(builder -> builder.subject("hexlet@Example.com"));
+        testTask = Instancio.of(modelGenerator.getTaskModel())
+                .create();
+        taskRepository.save(testTask);
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        var request = get("/api/tasks").with(token);
+        var result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).isArray();
+        assertThat(body).contains(String.valueOf(testTask.getId()));
+        assertThat(body).contains(testTask.getName());
+        assertThat(body).contains(testTask.getDescription());
+    }
+}
