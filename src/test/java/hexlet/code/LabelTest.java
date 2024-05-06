83f9888c640b47d21a9a72bbba9b9490dd22a74e
error4071 <sergey_tsoy87@mail.ru>
2024-05-06 23:56:08 +0500

    LabelTest fix

--- src/test/java/hexlet/code/LabelTest.java
+++ src/test/java/hexlet/code/LabelTest.java
@@ -1,10 +1,63 @@
 package hexlet.code;
 
+import hexlet.code.model.Label;
+import hexlet.code.repository.LabelRepository;
+import hexlet.code.util.ModelGenerator;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class LabelTest {
 
+    @Autowired
+    private ModelGenerator modelGenerator;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Label testLabel;
+
+    @Autowired
+    private LabelRepository labelRepository;
+
+    private SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor token;
+
+    @BeforeEach
+    public void setUp() {
+        token = jwt().jwt(builder -> builder.subject("hexlet@Example.com"));
+        testLabel = Instancio.of(modelGenerator.getLabelModel())
+                .create();
+        labelRepository.save(testLabel);
+    }
+
+    @Test
+    private void testIndex() throws Exception {
+        var request = get("/api/labels").with(token);
+        var result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).isArray();
+        assertThat(body).contains(String.valueOf(testLabel.getId()));
+        assertThat(body).contains(testLabel.getName());
+    }
 }
