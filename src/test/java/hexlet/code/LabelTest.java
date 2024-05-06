2ca2340ff6e4d3900b1b1fb912a2d5444212df20
error4071 <sergey_tsoy87@mail.ru>
2024-05-07 00:37:56 +0500

    unused imports delete

--- src/test/java/hexlet/code/LabelTest.java
+++ src/test/java/hexlet/code/LabelTest.java
@@ -18,7 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatObject;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
