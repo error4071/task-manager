02519123b3b108b1f4a62450c26a941541adf20c
error4071 <sergey_tsoy87@mail.ru>
2024-05-08 22:40:20 +0500

    labelTest edit

--- src/test/java/hexlet/code/TaskStatusesTest.java
+++ src/test/java/hexlet/code/TaskStatusesTest.java
@@ -3,22 +3,23 @@ package hexlet.code;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import hexlet.code.dto.TaskStatus.TaskStatusCreateDTO;
+import hexlet.code.dto.TaskStatus.TaskStatusUpdateDTO;
 import hexlet.code.mapper.TaskStatusMapper;
 import hexlet.code.model.TaskStatus;
 import hexlet.code.repository.TaskStatusRepository;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -26,6 +27,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import hexlet.code.util.ModelGenerator;
 import net.datafaker.Faker;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.util.Optional;
 
@@ -98,6 +100,27 @@ public class TaskStatusesTest {
 
         assertThat(taskStatusTest).isNotNull();
         assertThat(taskStatusTest.getName()).isEqualTo(taskStatusCreateDTO.getName());
+    }
+
+    public void testUpdate() throws Exception {
+        taskStatusRepository.save(testTaskStatus);
+        TaskStatusUpdateDTO taskStatusUpdateDTO = new TaskStatusUpdateDTO();
+
+            taskStatusUpdateDTO.setName(JsonNullable.of(faker.internet().emailAddress()));
+            taskStatusUpdateDTO.setSlug(JsonNullable.of(faker.internet().slug()));
+
+        MockHttpServletRequestBuilder request = put("/api/labels/{id}", testTaskStatus.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(objectMapper.writeValueAsString(taskStatusUpdateDTO))
+                .with(SecurityMockMvcRequestPostProcessors.user(testTaskStatus.getName()));
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+
+        var updatedTaskStatus = taskStatusRepository.findBySlug(testTaskStatus.getSlug()).get();
+
+        assertThat(updatedTaskStatus).isNotNull();
+        assertThat(updatedTaskStatus.getName()).isEqualTo(taskStatusUpdateDTO.getName());
     }
 
     @Test
