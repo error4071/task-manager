a3704f9924683c966868596af0310ea9d279b85e
error4071 <sergey_tsoy87@mail.ru>
2024-05-19 18:13:54 +0500

    Test fix

--- src/main/java/hexlet/code/dto/Label/LabelUpdateDTO.java
+++ src/main/java/hexlet/code/dto/Label/LabelUpdateDTO.java
@@ -1,17 +1,15 @@
 package hexlet.code.dto.Label;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.openapitools.jackson.nullable.JsonNullable;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class LabelUpdateDTO {
-    @NotBlank
+    @NotNull
     @Size(min = 3, max = 1000)
     private JsonNullable<String> name;
 }
