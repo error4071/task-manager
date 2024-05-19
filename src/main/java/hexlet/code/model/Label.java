537c45a004c78f5bda93406737a6a826c4de8739
error4071 <sergey_tsoy87@mail.ru>
2024-05-19 17:59:21 +0500

    Test fix

--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -1,6 +1,5 @@
 package hexlet.code.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
