c6d8215fc5671d83f0f38c3d0499ae4fcc25dbf1
error4071 <sergey_tsoy87@mail.ru>
2024-05-19 22:03:19 +0500

    aaa

--- src/test/java/hexlet/code/UserTest.java
+++ src/test/java/hexlet/code/UserTest.java
@@ -20,6 +20,7 @@ import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
@@ -35,6 +36,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Configuration
 public class UserTest {
 
     @Autowired
