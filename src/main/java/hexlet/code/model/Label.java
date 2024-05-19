f729b10ea6443f66233dd867e20146b8fd67b8e2
error4071 <sergey_tsoy87@mail.ru>
2024-05-19 17:35:24 +0500

    1

--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -1,14 +1,16 @@
 package hexlet.code.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
@@ -16,6 +18,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "labels")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public class Label implements BaseEntity {
