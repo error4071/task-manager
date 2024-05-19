246441709a14eaf3a7bc6ede329a79435b1f7b65
error4071 <sergey_tsoy87@mail.ru>
2024-05-19 20:17:53 +0500

    Task test update

--- src/test/java/hexlet/code/TaskTest.java
+++ src/test/java/hexlet/code/TaskTest.java
@@ -11,11 +11,23 @@ import hexlet.code.util.ModelGenerator;
 import net.datafaker.Faker;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 
@@ -57,8 +69,47 @@ public class TaskTest {
     @BeforeEach
     public void setUp() {
         token = jwt().jwt(builder -> builder.subject("hexlet@Example.com"));
+
+        var userTest = userRepository.findByEmail("hexlet@Example.com")
+                .orElseThrow(() -> new RuntimeException("User not found."));
+
+        var taskStatusTest = taskStatusRepository.findBySlug("draft")
+                .orElseThrow(() -> new RuntimeException("TaskStatus not found."));
+
+        var labelTest = labelRepository.findByName("feature")
+                .orElseThrow(() -> new RuntimeException("Label not found."));
+
         testTask = Instancio.of(modelGenerator.getTaskModel())
                 .create();
+        testTask.setAssignee(userTest);
+        testTask.setTaskStatus(taskStatusTest);
         taskRepository.save(testTask);
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        var data = Map.of(
+                "index", (Integer) faker.number().positive(),
+                "assignee_id", 1L,
+                "title", faker.lorem().word(),
+                "content", faker.lorem().sentence(),
+                "status", "draft"
+        );
+
+        var request = post("/api/tasks").with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated());
+
+        var task = taskRepository.findByName((String) data.get("title")).orElse(null);
+
+        assertThat(task).isNotNull();
+        assertThat(task.getIndex()).isEqualTo(data.get("index"));
+        assertThat(task.getAssignee().getId()).isEqualTo(data.get("assignee_id"));
+        assertThat(task.getName()).isEqualTo(data.get("title"));
+        assertThat(task.getDescription()).isEqualTo(data.get("content"));
+        assertThat(task.getTaskStatus()).isEqualTo(data.get("draft"));
     }
 }
