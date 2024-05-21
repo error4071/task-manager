d4575a0d336e368517c7f2b1956e0571694e62aa
error4071 <sergey_tsoy87@mail.ru>
2024-05-21 21:36:50 +0500

    ModelGenerator fix

--- src/test/java/hexlet/code/util/ModelGenerator.java
+++ src/test/java/hexlet/code/util/ModelGenerator.java
@@ -37,10 +37,11 @@ public class ModelGenerator {
                 .supply(Select.field(Label::getName), () -> "new")
                 .toModel();
 
-        taskStatusModel = Instancio.of(TaskStatus.class)
+        taskStatusModel = (Model<TaskStatus>) Instancio.of(TaskStatus.class)
                 .ignore(Select.field(TaskStatus::getId))
-                .supply(Select.field(TaskStatus::getName), () -> "To test")
-                .supply(Select.field(TaskStatus::getSlug), () -> "to_test")
-                .toModel();
+                .ignore(Select.field(TaskStatus::getCreatedAt))
+                .supply(Select.field(TaskStatus::getName), () -> faker.lorem().word())
+                .supply(Select.field(TaskStatus::getSlug), () -> faker.lorem().word())
+                .create();
     }
 }
