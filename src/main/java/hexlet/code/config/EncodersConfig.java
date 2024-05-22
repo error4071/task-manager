c8c836280966b012445802cc1be9d106f9311ab5
error4071 <sergey_tsoy87@mail.ru>
2024-05-22 22:42:48 +0500

    edit

--- src/main/java/hexlet/code/config/EncodersConfig.java
+++ src/main/java/hexlet/code/config/EncodersConfig.java
@@ -24,6 +24,7 @@ import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 @AllArgsConstructor
 public class EncodersConfig {
 
+    @Autowired
     private RsaKeyProperties keyProperties;
 
     //Хэшируем пароль
