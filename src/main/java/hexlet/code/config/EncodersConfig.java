eb2e527fab3959783a1690b244d097649776ecc2
error4071 <sergey_tsoy87@mail.ru>
2024-05-22 20:19:41 +0500

    EncodersConfix minor edit

--- src/main/java/hexlet/code/config/EncodersConfig.java
+++ src/main/java/hexlet/code/config/EncodersConfig.java
@@ -6,7 +6,10 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+
 import hexlet.code.component.RsaKeyProperties;
+
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +21,9 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 
 @Configuration
+@AllArgsConstructor
 public class EncodersConfig {
-
-    @Autowired
+    
     private RsaKeyProperties keyProperties;
 
     //Хэшируем пароль
