e5849305945be22dc7381135c9a058b6f0e384bd
error4071 <sergey_tsoy87@mail.ru>
2024-05-23 16:34:57 +0500

    Date fix

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -6,24 +6,20 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
-import jakarta.persistence.Temporal;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
-
-import static jakarta.persistence.TemporalType.TIMESTAMP;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
@@ -46,10 +42,10 @@ public class User implements UserDetails, BaseEntity {
     private String email;
 
     @CreatedDate
-    private Date createdAt;
+    private LocalDate createdAt;
 
     @CreatedDate
-    private Date updatedAt;
+    private LocalDate updatedAt;
 
     @NotBlank
     private String passwordDigest;
