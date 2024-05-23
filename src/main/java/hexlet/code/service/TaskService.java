53b4019ba7bb9491dfcb0f709b76f950677b3b71
error4071 <sergey_tsoy87@mail.ru>
2024-05-23 22:40:16 +0500

    TaskService @AllArgs delete

--- src/main/java/hexlet/code/service/TaskService.java
+++ src/main/java/hexlet/code/service/TaskService.java
@@ -7,7 +7,6 @@ import hexlet.code.dto.Task.TaskUpdateDTO;
 import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.TaskMapper;
 import hexlet.code.repository.TaskRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
