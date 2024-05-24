11947d2c8db4f7d53c328db2d9a0eace422be315
error4071 <sergey_tsoy87@mail.ru>
2024-05-24 16:06:48 +0500

    TaskStatusDto fix

--- src/main/java/hexlet/code/dto/TaskStatus/TaskStatusDTO.java
+++ src/main/java/hexlet/code/dto/TaskStatus/TaskStatusDTO.java
@@ -1,5 +1,6 @@
 package hexlet.code.dto.TaskStatus;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class TaskStatusDTO {
     private Long id;
     private String name;
