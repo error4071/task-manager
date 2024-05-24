9bd6f6e632744954422880508b9fb53c42318523
error4071 <sergey_tsoy87@mail.ru>
2024-05-24 17:41:46 +0500

    Fix

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -13,7 +13,6 @@ import hexlet.code.repository.LabelRepository;
 import hexlet.code.repository.TaskRepository;
 import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.UserRepository;
-import hexlet.code.ModelGenerator;
 import lombok.AllArgsConstructor;
 import org.instancio.Instancio;
 import org.instancio.Select;
@@ -21,8 +20,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import org.apache.commons.text.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -49,46 +46,13 @@ public class DataInitializer implements ApplicationRunner {
 
     private Task task;
 
-    private ModelGenerator modelGenerator;
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-        addModels();
-    }
-
-    private void addModels() {
         var userData = new UserCreateDTO();
         userData.setEmail("hexlet@example.com");
-        userData.setPassword("qwerty");
+        userData.setPassword(passwordEncoder.encode("qwerty"));
         var user = userMapper.map(userData);
         userRepository.save(user);
-
-        var labelNames = Arrays.asList("bug", "feature");
-        labelNames.stream()
-                .map(name -> {
-                    var label = new Label();
-                    label.setName(name);
-                    labelRepository.save(label);
-                    return label;
-                }).toList();
-
-        var statusNames = Arrays.asList("draft", "to_review", "to_be_fixed", "to_publish", "published");
-        var taskStatuses = statusNames.stream()
-                .map(name -> {
-                    var taskStatus = new TaskStatus();
-                    taskStatus.setSlug(name);
-                    taskStatus.setName(CaseUtils.toCamelCase(name, true, new char[] { '_' }));
-                    taskStatusRepository.save(taskStatus);
-                    return taskStatus;
-                }).toList();
-
-        var tasks = Instancio.ofList(modelGenerator.getTaskModel())
-                .size(10)
-                .generate(Select.field(Task::getTaskStatus), gen -> gen.oneOf(taskStatuses))
-                .supply(Select.field(Task::getAssignee), () -> user)
-                .create();
-        taskRepository.saveAll(tasks);
     }
 
     public void addSlug() {
