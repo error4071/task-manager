1f567c3ecc606a91e3ad960ff5eca83df7410929
error4071 <sergey_tsoy87@mail.ru>
2024-05-26 21:31:06 +0500

    ModelGenerator fix

--- src/test/java/hexlet/code/util/ModelGenerator.java
+++ src/test/java/hexlet/code/util/ModelGenerator.java
@@ -1,31 +1,52 @@
 package hexlet.code.util;
 
+import hexlet.code.model.Task;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.model.User;
+import hexlet.code.model.Label;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.datafaker.Faker;
 import org.instancio.Instancio;
 import org.instancio.Model;
 import org.instancio.Select;
-
-import hexlet.code.model.Task;
-import hexlet.code.model.User;
-import lombok.Getter;
-import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Getter
+@AllArgsConstructor
 public class ModelGenerator {
-    private Model<Task> taskModel;
+
     private Model<User> userModel;
+    private Model<Task> taskModel;
+    private Model<TaskStatus> taskStatusModel;
+    private Model<Label> labelModel;
+
+    @Autowired
+    private Faker faker;
 
     public ModelGenerator() {
-        var faker = new Faker();
         userModel = Instancio.of(User.class)
                 .ignore(Select.field(User::getId))
-                .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
                 .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
+                .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
                 .toModel();
 
         taskModel = Instancio.of(Task.class)
                 .ignore(Select.field(Task::getId))
-                .supply(Select.field(Task::getName), () -> faker.gameOfThrones().house())
-                .supply(Select.field(Task::getDescription), () -> faker.gameOfThrones().quote())
+                .supply(Select.field(Task::getName), () -> faker.lorem().word())
+                .supply(Select.field(Task::getDescription), () -> faker.lorem().sentence())
+                .toModel();
+
+        labelModel = Instancio.of(Label.class)
+                .ignore(Select.field(Label::getId))
+                .supply(Select.field(Label::getName), () -> faker.text().text(3, 1000))
+                .toModel();
+
+        taskStatusModel = Instancio.of(TaskStatus.class)
+                .ignore(Select.field(TaskStatus::getId))
+                .ignore(Select.field(TaskStatus::getCreatedAt))
+                .supply(Select.field(TaskStatus::getName), () -> faker.lorem().word())
+                .supply(Select.field(TaskStatus::getSlug), () -> faker.lorem().sentence())
                 .toModel();
     }
 }
