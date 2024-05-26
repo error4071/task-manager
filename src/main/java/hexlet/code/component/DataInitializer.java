e96adefa8c596e3346e80eacd4122f49556d5875
error4071 <sergey_tsoy87@mail.ru>
2024-05-26 20:54:35 +0500

    TaskTest fix

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,126 +1,64 @@
 package hexlet.code.component;
 
-import hexlet.code.ModGen;
-import hexlet.code.dto.Label.LabelCreateDTO;
-import hexlet.code.dto.TaskStatus.TaskStatusCreateDTO;
-import hexlet.code.dto.User.UserCreateDTO;
-import hexlet.code.mapper.LabelMapper;
-import hexlet.code.mapper.TaskStatusMapper;
-import hexlet.code.mapper.UserMapper;
 import hexlet.code.model.Label;
-import hexlet.code.model.Task;
 import hexlet.code.model.TaskStatus;
+import hexlet.code.model.User;
 import hexlet.code.repository.LabelRepository;
-import hexlet.code.repository.TaskRepository;
 import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.apache.commons.text.CaseUtils;
-import org.instancio.Instancio;
-import org.instancio.Select;
+import hexlet.code.service.CustomUserDetailsService;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
 @Component
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
-
-    private final UserRepository userRepository;
-
+    private final CustomUserDetailsService userService;
     private final TaskStatusRepository taskStatusRepository;
-
+    private final UserRepository userRepository;
     private final LabelRepository labelRepository;
 
-    private final UserMapper userMapper;
-
-    private final TaskStatusMapper taskStatusMapper;
-
-    private final LabelMapper labelMapper;
-
-    private PasswordEncoder passwordEncoder;
-
-    private final TaskRepository taskRepository;
-
-    private final Task task;
-
-    private final ModGen modGen = new ModGen();
-
-
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-
-        addModels();
-        addSlug();
-        addLabel();
-    }
-
-    private void addModels() {
-        var userData = new UserCreateDTO();
-        userData.setEmail("hexlet@example.com");
-        userData.setPassword("qwerty");
-        var user = userMapper.map(userData);
-        userRepository.save(user);
-
-        var labelNames = Arrays.asList("bug", "feature");
-        labelNames.stream()
-                .map(name -> {
-                    var label = new Label();
-                    label.setName(name);
-                    labelRepository.save(label);
-                    return label;
-                }).toList();
-
-        var statusNames = Arrays.asList("draft", "to_review", "to_be_fixed", "to_publish", "published");
-        var taskStatuses = statusNames.stream()
-                .map(name -> {
-                    var taskStatus = new TaskStatus();
-                    taskStatus.setSlug(name);
-                    taskStatus.setName(CaseUtils.toCamelCase(name, true, new char[] { '_' }));
-                    taskStatusRepository.save(taskStatus);
-                    return taskStatus;
-                }).toList();
-
-        var tasks = Instancio.ofList(modGen.getTaskModel())
-                .size(10)
-                .generate(Select.field(Task::getTaskStatus), gen -> gen.oneOf(taskStatuses))
-                .supply(Select.field(Task::getAssignee), () -> user)
-                .create();
-        taskRepository.saveAll(tasks);
-    }
-
-    public void addSlug() {
-        List<String> defaultSlug = List.of("draft", "to_review", "to_be_fixed", "to_publish", "published");
-        defaultSlug.forEach(slug -> {
-            var statusData = new TaskStatusCreateDTO();
-            String[] arr = slug.split("_");
-            String data = arr[0].substring(0, 1).toUpperCase() + arr[0].substring(1);
-            var name = new StringBuilder(String.valueOf(data));
-
-            if (arr.length > 1) {
-                for (var element: arr) {
-                    name.append(" ").append(element);
-                }
-            }
-
-            statusData.setName(name.toString());
-            statusData.setSlug(slug.toString());
-            var status = taskStatusMapper.map(statusData);
+    public void run(ApplicationArguments args) {
+        User admin = new User();
+        admin.setEmail("hexlet@example.com");
+        admin.setPasswordDigest("qwerty");
+        var statuses = createTaskStatuses();
+        if (userRepository.findByEmail("hexlet@example.com").isEmpty()) {
+            userService.createUser(admin);
+        }
+        for (var status: statuses) {
             taskStatusRepository.save(status);
-        });
+        }
+        var label1 = new Label();
+        label1.setName("bug");
+        labelRepository.save(label1);
+        var label2 = new Label();
+        label2.setName("feature");
+        labelRepository.save(label2);
     }
 
-    public void addLabel() {
-        List<String> labels = List.of("feature", "bug");
-        labels.forEach(name -> {
-            var labelData = new LabelCreateDTO();
-            labelData.setName(name);
-            var label = labelMapper.map(labelData);
-            labelRepository.save(label);
-        });
+    private static List<TaskStatus> createTaskStatuses() {
+        TaskStatus draft = new TaskStatus();
+        draft.setName("Draft");
+        draft.setSlug("draft");
+        TaskStatus toReview = new TaskStatus();
+        toReview.setName("To Review");
+        toReview.setSlug("to_review");
+        TaskStatus toBeFixed = new TaskStatus();
+        toBeFixed.setName("To Be Fixed");
+        toBeFixed.setSlug("to_be_fixed");
+        TaskStatus toPublish = new TaskStatus();
+        toPublish.setName("To Publish");
+        toPublish.setSlug("to_publish");
+        TaskStatus published = new TaskStatus();
+        published.setName("Published");
+        published.setSlug("published");
+        return List.of(draft, toReview, toBeFixed, toPublish, published);
     }
 }
