590214294748067636d022b69d1d0cef012aa390
error4071 <sergey_tsoy87@mail.ru>
2024-05-30 01:25:07 +0500

    update

--- src/main/java/hexlet/code/model/TaskStatus.java
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -1,42 +1,56 @@
 package hexlet.code.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.GeneratedValue;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-@Table(name = "task_statuses")
-@Getter
 @Setter
-
-public class TaskStatus implements BaseEntity {
-
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@ToString(includeFieldNames = true, onlyExplicitlyIncluded = true)
+@Table(name = "task_statuses")
+@EqualsAndHashCode
+public class TaskStatus {
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private Long id;
 
+    @ToString.Include
+    @NotNull
+    @Size(min = 1)
     @Column(unique = true)
-    @NotBlank
     private String name;
 
+    @ToString.Include
     @Column(unique = true)
-    @NotNull
+    @Size(min = 1)
     private String slug;
 
     @CreatedDate
     private LocalDate createdAt;
+
+    @OneToMany(mappedBy = "taskStatus", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    private List<Task> tasks;
 }
