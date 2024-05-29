9c0f7bcf02982cdd9732d40dcde2def2114fd0c6
error4071 <sergey_tsoy87@mail.ru>
2024-05-29 23:35:47 +0500

    TaskStatus edit

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -1,14 +1,6 @@
 package hexlet.code.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -43,6 +35,7 @@ public class Task implements BaseEntity {
 
     @ManyToOne
     @NotNull
+    @OneToMany(cascade = CascadeType.MERGE)
     private TaskStatus taskStatus;
 
     @ManyToOne(fetch = FetchType.EAGER)
--- src/main/java/hexlet/code/model/TaskStatus.java
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -1,40 +1,52 @@
 package hexlet.code.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
-@Table(name = "task_statuses")
-@EntityListeners(AuditingEntityListener.class)
-@Getter
 @Setter
-public class TaskStatus implements BaseEntity {
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "task_statuses")
+@EqualsAndHashCode
 
+public class TaskStatus implements BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
+    @NotNull
+    @Size(min = 1)
     @Column(unique = true)
-    @NotBlank
     private String name;
 
     @Column(unique = true)
-    @NotBlank
+    @Size(min = 1)
     private String slug;
 
     @CreatedDate
     private LocalDate createdAt;
+
+    @OneToMany(mappedBy = "taskStatus", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    private List<Task> tasks;
 }
