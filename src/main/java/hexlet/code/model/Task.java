9afd9081f6038d39abbbb64be34d2d2a5007092a
error4071 <sergey_tsoy87@mail.ru>
2024-05-30 01:27:30 +0500

    update

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -1,27 +1,32 @@
 package hexlet.code.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "tasks")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
-
 public class Task implements BaseEntity {
 
     @Id
@@ -31,9 +36,9 @@ public class Task implements BaseEntity {
     private Integer index;
 
     @NotBlank
-    @Size(min = 1)
     private String name;
 
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @ManyToOne
@@ -43,7 +48,10 @@ public class Task implements BaseEntity {
     @ManyToOne(fetch = FetchType.EAGER)
     private User assignee;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @NotNull
+    private Set<Label> labels = new HashSet<>();
+
     @CreatedDate
     private LocalDate createdAt;
-
 }
