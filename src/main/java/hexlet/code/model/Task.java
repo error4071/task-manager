56d846065195f17a2de5769f9265b436965b79df
error4071 <sergey_tsoy87@mail.ru>
2024-06-02 17:19:02 +0500

    Fix

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -1,49 +1,54 @@
 package hexlet.code.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
+import java.util.List;
 
 @Entity
 @Table(name = "tasks")
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
+@ToString
 public class Task implements BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Integer index;
-
-    @NotBlank
+    @Size(min = 1)
     private String name;
 
-    @Column(columnDefinition = "TEXT")
+    private int index;
+
     private String description;
 
-    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
-    @NotNull
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private TaskStatus taskStatus;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User assignee;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @NotNull
-    private Set<Label> labels = new HashSet<>();
+    @ManyToMany()
+    private List<Label> labels;
 
     @CreatedDate
     private LocalDate createdAt;
+
 }
