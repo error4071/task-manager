b9fb966f380343cb96e541a731cd27eccb716863
error4071 <sergey_tsoy87@mail.ru>
2024-06-02 16:39:18 +0500

    Fix

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -42,7 +42,7 @@ public class Task implements BaseEntity {
     @Column(columnDefinition = "TEXT")
     private String description;
 
-    @ManyToOne(cascade = CascadeType.MERGE)
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
     @NotNull
     private TaskStatus taskStatus;
 
