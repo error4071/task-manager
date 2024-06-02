a6e0ee28aeb285a8d82dca374689f58684f3e8dd
error4071 <sergey_tsoy87@mail.ru>
2024-06-02 17:34:44 +0500

    Fix

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -1,10 +1,20 @@
 package hexlet.code.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -12,35 +22,41 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 @Entity
 @Table(name = "tasks")
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Task implements BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @ToString.Include
+    @EqualsAndHashCode.Include
     private Long id;
 
-    private Integer index;
-
     @NotBlank
+    @ToString.Include
     private String name;
 
-    @Column(columnDefinition = "TEXT")
+    @ToString.Include
+    private int index;
+
+    @ToString.Include
     private String description;
 
-    @ManyToOne()
+    @ToString.Include
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
     private TaskStatus taskStatus;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ToString.Include
+    @ManyToOne(fetch = FetchType.LAZY)
     private User assignee;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @NotNull
+    @ManyToMany
     private Set<Label> labels = new HashSet<>();
 
     @CreatedDate
--- src/main/java/hexlet/code/model/TaskStatus.java
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -1,47 +1,55 @@
 package hexlet.code.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
 import jakarta.persistence.Column;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 @Entity
-@Setter
-@Getter
-@EntityListeners(AuditingEntityListener.class)
 @Table(name = "task_statuses")
-
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class TaskStatus implements BaseEntity {
-
     @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @ToString.Include
+    @EqualsAndHashCode.Include
     private Long id;
 
-    @NotNull
     @Size(min = 1)
     @Column(unique = true)
+    @NotBlank
+    @ToString.Include
     private String name;
 
-    @Column(unique = true)
     @Size(min = 1)
+    @Column(unique = true)
+    @NotBlank
+    @ToString.Include
     private String slug;
 
     @CreatedDate
     private LocalDate createdAt;
+
+    @OneToMany(mappedBy = "taskStatus")
+    private List<Task> tasks = new ArrayList<>();
 }
