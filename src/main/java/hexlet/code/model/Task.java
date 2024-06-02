a237f172bde8e0c1f2c7d966b4b851b06f26ce08
error4071 <sergey_tsoy87@mail.ru>
2024-06-02 17:39:45 +0500

    Fix

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -1,20 +1,10 @@
 package hexlet.code.model;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -22,41 +12,36 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 @Entity
 @Table(name = "tasks")
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
-@ToString(onlyExplicitlyIncluded = true)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Task implements BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @ToString.Include
-    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
+    private Integer index;
+
     @NotBlank
-    @ToString.Include
     private String name;
 
-    @ToString.Include
-    private int index;
-
-    @ToString.Include
+    @Column(columnDefinition = "TEXT")
     private String description;
 
-    @ToString.Include
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.MERGE)
     @NotNull
     private TaskStatus taskStatus;
 
-    @ToString.Include
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     private User assignee;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
+    @NotNull
     private Set<Label> labels = new HashSet<>();
 
     @CreatedDate
