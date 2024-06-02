8d0d60c75b6e94a15d5ed2f05013e184bb1ea3c3
error4071 <sergey_tsoy87@mail.ru>
2024-06-02 16:33:24 +0500

    Fix

--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -3,24 +3,16 @@ package hexlet.code.mapper;
 import hexlet.code.dto.Task.TaskCreateDTO;
 import hexlet.code.dto.Task.TaskDTO;
 import hexlet.code.dto.Task.TaskUpdateDTO;
-import hexlet.code.model.Label;
 import hexlet.code.model.Task;
-import hexlet.code.repository.LabelRepository;
+
 import lombok.Getter;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.MappingTarget;
 
 @Getter
 @Mapper(
@@ -29,19 +21,13 @@ import java.util.Set;
         componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
+
 public abstract class TaskMapper {
 
-    @Autowired
-    private LabelRepository labelRepository;
-
-    private final String defaultContent = "";
-
     @Mapping(target = "assignee", source = "assigneeId")
-    @Mapping(target = "taskStatus.slug", source = "status")
-    @Mapping(target = "labels", source = "taskLabelIds")
     @Mapping(target = "name", source = "title")
-    @Mapping(target = "description",
-            expression = "java(dto.getContent() == null ? getDefaultContent() : dto.getContent())")
+    @Mapping(target = "taskStatus.slug", source = "status")
+    @Mapping(target = "description", source = "content")
     public abstract Task map(TaskCreateDTO dto);
 
     @Mapping(source = "assignee.id", target = "assigneeId")
