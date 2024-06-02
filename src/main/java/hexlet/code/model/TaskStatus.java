4106addcf387f8f064919479c8d81987fc9cac95
error4071 <sergey_tsoy87@mail.ru>
2024-06-02 21:44:33 +0500

    Fix

--- src/main/java/hexlet/code/model/TaskStatus.java
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -44,5 +44,6 @@ public class TaskStatus {
     @CreatedDate
     private LocalDate createdAt;
 
-
+    @OneToMany(mappedBy = "taskStatus", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    private List<Task> tasks;
 }
