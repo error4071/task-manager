54f508f39819f835491c80cbdf594f5c0ecad23e
error4071 <sergey_tsoy87@mail.ru>
2024-06-02 15:50:38 +0500

    Task edit

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -40,7 +40,7 @@ public class Task implements BaseEntity {
     @ManyToOne(fetch = FetchType.EAGER)
     private User assignee;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
     @NotNull
     private Set<Label> labels = new HashSet<>();
 
