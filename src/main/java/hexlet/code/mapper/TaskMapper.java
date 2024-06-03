97ac0e4d202ca99d908734799f8b52d01e44e9bf
error4071 <sergey_tsoy87@mail.ru>
2024-06-04 01:29:31 +0500

    TaskMapper fix

--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -63,10 +63,6 @@ public abstract class TaskMapper {
     @Mapping(target = "description", source = "content")
     public abstract void update(TaskUpdateDTO dto, @MappingTarget Task model);
 
-    public Set<Label> toLabelsSet(List<Long> taskLabelIds) {
-        return new HashSet<>(labelRepository.findByIdIn(taskLabelIds).orElse(new HashSet<>()));
-    }
-
     @Named("slugToTaskStatus")
     public TaskStatus toEntity(String slug) {
         var taskStatus = taskStatusRepository.findBySlug(slug)
