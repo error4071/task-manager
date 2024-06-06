7fdbcf8ac5ebf6e66e68e5c09aa2eb970d8cae13
error4071 <sergey_tsoy87@mail.ru>
2024-06-06 23:45:15 +0500

    Task fix

--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -52,7 +52,7 @@ public abstract class TaskMapper {
     @Mapping(source = "name", target = "title")
     @Mapping(source = "description", target = "content")
     @Mapping(target = "createdAt", source = "createdAt")
-    @Mapping(target = "labels", source = "taskLabelIds")
+    @Mapping(target = "taskLabelIds", source = "labels", qualifiedByName = "labelsToIds")
     public abstract TaskDTO map(Task model);
 
     @Mapping(target = "assignee", source = "assigneeId")
