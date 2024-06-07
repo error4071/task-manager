3be7364ca8c1c277f9113c4ebb0a52e991d3c35a
error4071 <sergey_tsoy87@mail.ru>
2024-06-08 00:54:01 +0500

    Fix

--- src/main/java/hexlet/code/dto/Task/TaskDTO.java
+++ src/main/java/hexlet/code/dto/Task/TaskDTO.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 @Getter
 @Setter
