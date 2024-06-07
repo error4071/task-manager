3819851e4ab2184879f6bb5d38c9626fb88f2fba
error4071 <sergey_tsoy87@mail.ru>
2024-06-07 23:58:35 +0500

    Fix

--- src/main/java/hexlet/code/dto/Task/TaskCreateDTO.java
+++ src/main/java/hexlet/code/dto/Task/TaskCreateDTO.java
@@ -1,8 +1,5 @@
 package hexlet.code.dto.Task;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
