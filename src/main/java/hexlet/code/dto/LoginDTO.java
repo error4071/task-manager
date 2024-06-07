93979dbd78f96ba2b91b4b621fded5168eadadfc
error4071 <sergey_tsoy87@mail.ru>
2024-06-07 22:52:47 +0500

    Fix

--- src/main/java/hexlet/code/dto/LoginDTO.java
+++ src/main/java/hexlet/code/dto/LoginDTO.java
@@ -2,11 +2,9 @@ package hexlet.code.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class LoginDTO {
     private String username;
     private String password;
