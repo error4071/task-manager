c7fdddfd9189dd941c7d5b9c7ddea665e18763b0
error4071 <sergey_tsoy87@mail.ru>
2024-06-08 01:20:01 +0500

    Fix

--- src/main/java/hexlet/code/dto/Task/TaskDTO.java
+++ src/main/java/hexlet/code/dto/Task/TaskDTO.java
@@ -1,6 +1,5 @@
 package hexlet.code.dto.Task;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,12 +10,9 @@ import java.util.Date;
 public class TaskDTO {
 
     private Long id;
-    private Integer index;
+    private int index;
     private Date createdAt;
-
-    @JsonProperty("assignee_id")
     private Long assigneeId;
-
     private String title;
     private String content;
     private String status;
