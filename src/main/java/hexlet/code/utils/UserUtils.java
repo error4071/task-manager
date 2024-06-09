017dcdf7f2ef52efa18546a27f8d0a3136abd1a7
error4071 <sergey_tsoy87@mail.ru>
2024-06-09 22:45:10 +0500

    Fix

--- src/main/java/hexlet/code/utils/UserUtils.java
+++ src/main/java/hexlet/code/utils/UserUtils.java
@@ -3,6 +3,7 @@ package hexlet.code.utils;
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,8 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class UserUtils {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public User getCurrentUser() {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -19,16 +21,5 @@ public class UserUtils {
         }
         var email = authentication.getName();
         return userRepository.findByEmail(email).get();
-    }
-
-    public User getTestUser() {
-        return userRepository.findByEmail("hexlet@example.com")
-                .orElseThrow(() -> new RuntimeException("User doesn't exists"));
-    }
-
-    public boolean isUser(Long userId) {
-        var taskAuthorId = userRepository.findById(userId).get().getUsername();
-        var authentication = SecurityContextHolder.getContext().getAuthentication();
-        return taskAuthorId.equals(authentication.getName());
     }
 }
