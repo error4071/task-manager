455ba411ab747d89ec4401a5d90fe0c556ebfd3c
error4071 <sergey_tsoy87@mail.ru>
2024-06-15 13:47:42 +0500

    TaskMapper edit

--- src/main/java/hexlet/code/dto/Label/LabelDTO.java
+++ src/main/java/hexlet/code/dto/Label/LabelDTO.java
@@ -1,5 +1,6 @@
 package hexlet.code.dto.Label;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDate;
@@ -9,5 +10,7 @@ import java.time.LocalDate;
 public class LabelDTO {
     private Long id;
     private String name;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate createdAt;
 }
--- src/main/java/hexlet/code/dto/Task/TaskDTO.java
+++ src/main/java/hexlet/code/dto/Task/TaskDTO.java
@@ -3,6 +3,7 @@ package hexlet.code.dto.Task;
 import lombok.Getter;
 import lombok.Setter;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,7 +13,10 @@ import java.util.Set;
 public class TaskDTO {
     private Long id;
     private int index;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date createdAt;
+
     private Long assigneeId;
     private String title;
     private String content;
