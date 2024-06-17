9cca961347221fadef4a0fff5a47afc6797972a3
error4071 <sergey_tsoy87@mail.ru>
2024-06-18 00:06:18 +0500

    UserUtils fix

--- src/main/java/hexlet/code/utils/UserUtils.java
+++ src/main/java/hexlet/code/utils/UserUtils.java
@@ -3,15 +3,15 @@ package hexlet.code.utils;
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class UserUtils {
-    @Autowired
-    private UserRepository userRepository;
+
+    private final UserRepository userRepository;
 
     @Bean
     public User getCurrentUser() {
