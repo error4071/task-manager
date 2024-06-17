6cc6bb8a0330008de9f801ab4612ced9a7374ce4
error4071 <sergey_tsoy87@mail.ru>
2024-06-18 00:20:05 +0500

    UserUtils fix

--- src/main/java/hexlet/code/controller/api/UserController.java
+++ src/main/java/hexlet/code/controller/api/UserController.java
@@ -68,7 +68,7 @@ public class UserController {
     @PutMapping("/users/{id}")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Update user data")
-    @PreAuthorize("@userUtils.isUser(#id)")
+
     public UserDTO update(@PathVariable Long id, @RequestBody UserUpdateDTO userUpdateDTO) {
         return userService.update(id, userUpdateDTO);
     }
@@ -76,7 +76,7 @@ public class UserController {
     @DeleteMapping("/users/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Operation(summary = "Delete user")
-    @PreAuthorize("@userUtils.isUser(#id)")
+
     public void delete(@PathVariable Long id) {
         userRepository.deleteById(id);
     }
