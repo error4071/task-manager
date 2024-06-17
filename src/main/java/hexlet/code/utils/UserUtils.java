94bc9cbee7816f2276fd3145596811d4df07e939
error4071 <sergey_tsoy87@mail.ru>
2024-06-17 22:46:42 +0500

    UserController access fix

--- src/main/java/hexlet/code/utils/UserUtils.java
+++ src/main/java/hexlet/code/utils/UserUtils.java
@@ -3,6 +3,7 @@ package hexlet.code.utils;
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -12,13 +13,13 @@ public class UserUtils {
 
     private final UserRepository userRepository;
 
+    @Bean
     public User getCurrentUser() {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
             return null;
         }
-        var email = authentication.getName();
-        return userRepository.findByEmail(email).get();
+        return userRepository.findByEmail(authentication.getName()).orElseThrow();
     }
 
     public boolean isUser(Long id) {
