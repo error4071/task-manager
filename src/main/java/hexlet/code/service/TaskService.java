a04f6fb1d1da0c0017d79dab84b73458ce97c639
error4071 <sergey_tsoy87@mail.ru>
2024-06-19 22:15:32 +0500

    TaskService fix

--- src/main/java/hexlet/code/service/TaskService.java
+++ src/main/java/hexlet/code/service/TaskService.java
@@ -8,6 +8,7 @@ import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.TaskMapper;
 import hexlet.code.repository.TaskRepository;
 import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
 import hexlet.code.specification.TaskSpecification;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
@@ -20,6 +21,8 @@ import java.util.List;
 public class TaskService {
 
     private final TaskRepository taskRepository;
+
+    private final UserRepository userRepository;
 
     private final TaskStatusRepository taskStatusRepository;
 
@@ -37,6 +40,19 @@ public class TaskService {
 
     public TaskDTO create(TaskCreateDTO taskData) {
         var task = taskMapper.map(taskData);
+
+        var assigneeId = taskData.getAssigneeId();
+
+        if (assigneeId != null) {
+            var assignee = userRepository.findById(assigneeId).orElse(null);
+            task.setAssignee(assignee);
+        }
+
+        var statusSlug = taskData.getStatus();
+        var taskStatus = taskStatusRepository.findBySlug(statusSlug).orElse(null);
+
+        task.setTaskStatus(taskStatus);
+
         taskRepository.save(task);
         return taskMapper.map(task);
     }
