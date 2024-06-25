55b41ed1411122b88b8f5d65af158461e5c658fc
error4071 <sergey_tsoy87@mail.ru>
2024-06-26 00:34:19 +0500

    UserMapper edit

--- src/main/java/hexlet/code/mapper/UserMapper.java
+++ src/main/java/hexlet/code/mapper/UserMapper.java
@@ -9,11 +9,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.BeforeMapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(
         uses = {JsonNullableMapper.class, ReferenceMapper.class},
@@ -23,27 +20,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 )
 
 public abstract class UserMapper {
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
-    @Mapping(target = "passwordDigest", ignore = true)
-    public abstract void update(UserUpdateDTO dto, @MappingTarget User user);
     @Mapping(target = "passwordDigest", source = "password")
     public abstract User map(UserCreateDTO dto);
-    public abstract UserDTO map(User user);
 
-    @BeforeMapping
-    public void encryptPasswordUpdate(UserUpdateDTO dto, @MappingTarget User user) {
-        var password = dto.getPassword();
-        if (password != null && password.isPresent()) {
-            user.setPasswordDigest(passwordEncoder.encode(password.get()));
-        }
-    }
+    @Mapping(target = "createdAt", source = "createdAt")
+    public abstract UserDTO map(User model);
 
-    @BeforeMapping
-    public void encryptPasswordCreate(UserCreateDTO dto) {
-        var password = dto.getPassword();
-        dto.setPassword(passwordEncoder.encode(password));
-    }
+    @Mapping(target = "passwordDigest", source = "password")
+    public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
 }
-
