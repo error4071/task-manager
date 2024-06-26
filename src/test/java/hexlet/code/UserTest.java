cb01a37397a9662dcf6ada64874842ab269297b5
error4071 <sergey_tsoy87@mail.ru>
2024-06-27 00:15:36 +0500

    UserTest edit

--- src/test/java/hexlet/code/UserTest.java
+++ src/test/java/hexlet/code/UserTest.java
@@ -135,7 +135,7 @@ public class UserTest {
         assertThat(user.getEmail()).isEqualTo(data.get("email"));
         assertThat(user.getFirstName()).isEqualTo(data.get("firstName"));
         assertThat(user.getLastName()).isEqualTo(data.get("lastName"));
-        assertThat(user.getPassword()).isEqualTo(data.get("password"));
+        assertThat(user.getPasswordDigest()).isEqualTo(data.get("password"));
     }
     @Test
     public void testDestroy() throws Exception {
